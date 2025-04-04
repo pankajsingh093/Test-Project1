a4ae80aa3d5b8f3714273d33d733bc119537498e
pankajsingh093 <109786216+pankajsingh093@users.noreply.github.com>
2025-04-04 09:22:57 +0530

    customer login file added

--- src/main/java/org/qa/gittracker/Factory/DriverFactory.java
+++ src/main/java/org/qa/gittracker/Factory/DriverFactory.java
@@ -27,7 +27,7 @@ public class DriverFactory {
 		case "chrome":
 			ChromeOptions cp =new ChromeOptions();
 			cp.addArguments("--headless");
-			driver = new ChromeDriver();
+			driver = new ChromeDriver(cp);
 			break;
 		case "edge":
 			
