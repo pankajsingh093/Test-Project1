1eeffb210ef0afdda44395848868b3f04f1615c5
pankajsingh093 <109786216+pankajsingh093@users.noreply.github.com>
2025-04-27 14:15:08 +0530

    file added

--- src/test/java/org/qa/customer/tests/CustomerLoginPageTest.java
+++ src/test/java/org/qa/customer/tests/CustomerLoginPageTest.java
@@ -19,7 +19,7 @@ public class CustomerLoginPageTest extends BaseTest{
 	@Test(priority=2)
 	public void testCustomerLogin() throws InterruptedException {
 		//op = lp.doLogin(prop.getProperty("customerUN"), prop.getProperty("customerPASS"));
-		op = lp.doLogin(System.getProperty("username"), System.getProperty("password"));
+		lp.doLogin(System.getProperty("username"), System.getProperty("password"));
 		Thread.sleep(2000);
 		Assert.assertEquals(op.getOrderPageUrl(), GitTrackerConstants.CUST_OPEN_ORDER_PAGE_URL);
 	}
